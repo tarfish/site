576b73be68ff3a6f9afecb447be1b7441c32f3df
Sarvesh Neelraj <sarveshneel@gmail.com>
2025-12-05 15:19:43 +0400

    fixed projects page and added netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tarfish.github.io',
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: netlify()
 });
